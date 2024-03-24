1f075106033e30dff62c5412c0bf3142bd9a0d8d
shreyapalat2023 <garimapalat@gmail.com>
2024-03-24 08:31:24 +0530

    length

---
+++ 04ength.js
@@ -0,0 +1,10 @@
+a = [ 's', 'h', 'r', 2, 5, 6, "instagram"]
+b = "mango"
+c = "52"
+
+console.log(a.length)
+console.log(b.length)
+console.log(a[0])
+console.log(a.length-1)
+console.log(a[6])
+console.log(c.length)
